36d69857c2d71475a58ad6c42dabe02b7b90cfa4
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-20 17:07:39 +0300

    Task1_

---
+++ Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number = new Random().Next(100,1000);
+System.Console.WriteLine(number);
+
+int a1 = number%10;
+int a2 = number/10%10;
+int a3 = number/100%10; 
+System.Console.WriteLine(""+a3+""+a1);
