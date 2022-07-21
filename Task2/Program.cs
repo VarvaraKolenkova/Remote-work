f40d49a6ba6c34f3220eab9e1bfe3550755750b6
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-21 19:31:59 +0300

    Сортировка массива от макс к мин

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Алгоритм сортировки массива от max к min
+int [] arr = {8, 5, 8, 12, 9, 1, 3, 85};
+
+void PrintArray (int [] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+void SelectionSort(int [] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+SelectionSort(arr);
+PrintArray(arr);
+
