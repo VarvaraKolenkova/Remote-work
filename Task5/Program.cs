7f7f56a30d0ea3617dd738ebd27779f89c43e356
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-29 16:12:06 +0300

    ассив из 123 случайных чисел, считаем количество двузначных

---
+++ Task5/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] GetArrayRandom(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 150);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine(string.Join(", ", array));
+}
+
+int[] array = GetArrayRandom(123);
+PrintArray(array);
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 9 && array[i] < 100)
+    {
+        count+=1;
+    }
+}
+System.Console.WriteLine(count);
