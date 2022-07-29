8bde0268491110b51c2749e821c9178e9081240a
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-30 00:15:11 +0300

    x

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,9 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int[] array = {5, 8, 9, 11, 3, 14};
+
+double[] array = {5.1, 8.2, 9.9, 25.8, 3.14, 14.2};
+int[] array1 = ConventToInt32(double[] array);
 
 int FindMax(int[] array)
 {
-    int max = array[0];
+int max = array[0];
     for (int i = 0; i < array.Length; i++)
     {
         if(array[i] > max)
@@ -14,10 +16,9 @@ int FindMax(int[] array)
 return max;
 }
 System.Console.WriteLine(FindMax(array));
-
 int FindMin(int[] array)
 {
-    int min = array[0];
+int min = array[0];
     for (int i = 0; i < array.Length; i++)
     {
         if(array[i] < min)
