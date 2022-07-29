8ce2885628e40faf733a010f712ff623db844123
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-29 15:42:31 +0300

    Находим заданное число в массиве

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = {8, -5, 6, 5, 17, 3};
+
+int find = -5;
+foreach (var item in array)
+{
+    if(item == find) 
+    {
+        Console.WriteLine("Да"); 
+        return;
+    }    
+}
+Console.WriteLine("Нет");
+ 
