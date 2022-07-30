1771c429f3cbc1c2c2f06929d826f7b050b649c4
Varvara.Kolenkova <var.kolenkova@yandex.ru>
2022-07-30 07:19:31 +0300

    Разница между максимальным и минимальным элементом массива вещественных чисел

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,33 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 double[] array = {5.1, 8.2, 9.9, 25.8, 3.14, 14.2};
-int[] array1 = ConventToInt32(double[] array);
 
-int FindMax(int[] array)
+double FindMax(double[] array)
 {
-int max = array[0];
+double max = array[0];
     for (int i = 0; i < array.Length; i++)
     {
-        if(array[i] > max)
-        {
-            max = array[i];
-        }
+        if(array[i] > max) max = array[i];
     }
 return max;
 }
-System.Console.WriteLine(FindMax(array));
-int FindMin(int[] array)
+
+double FindMin(double[] array)
 {
-int min = array[0];
+double min = array[0];
     for (int i = 0; i < array.Length; i++)
     {
-        if(array[i] < min)
-        {
-            min = array[i];
-        }
+        if(array[i] < min) min = array[i];
     }
 return min;
 }
-System.Console.WriteLine(FindMin(array));
-
-System.Console.WriteLine(FindMax(array) - FindMin(array));
+System.Console.WriteLine($"Max element: {FindMax(array)}");
+System.Console.WriteLine($"Min element: {FindMin(array)}");
+System.Console.WriteLine($"Difference Max-Min: {FindMax(array) - FindMin(array)}");
